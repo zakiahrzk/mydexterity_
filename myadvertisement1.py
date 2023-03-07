8221ba861f211c0cf7e160cecd82fd5caaed8298
zakiahrzk <127167794+zakiahrzk@users.noreply.github.com>
2023-03-07 15:43:11 +0800

    Update myadvertisement1.py

--- myadvertisement1.py
+++ myadvertisement1.py
@@ -14,9 +14,9 @@ This app predicts the **Sales** !
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    TV = st.sidebar.slider('TV', 0.7, 296.4, 100)
-    Radio = st.sidebar.slider('Radio', 0.0, 49.6, 30)
-    Newspaper = st.sidebar.slider('Newspaper', 0.3, 114, 30.5)    
+    TV = st.sidebar.slider('TV', 10, 300, 150)
+    Radio = st.sidebar.slider('Radio', 0, 50, 30)
+    Newspaper = st.sidebar.slider('Newspaper', 0, 200, 45)    
     data = {'TV': TV,
             'Radio': Radio,
             'Newspaper': Newspaper}
@@ -39,7 +39,7 @@ Xtrain, Xtest, ytrain, ytest = train_test_split (X,y,test_size = 0.25, random_st
 linear = LinearRegression()
 linear.fit(Xtrain, ytrain)
 
-prediction = model.predict(df)
+prediction = linear.predict(df)
 
 st.subheader('Prediction')
 st.write(prediction)
