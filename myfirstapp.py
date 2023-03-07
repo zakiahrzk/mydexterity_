cfc4a13fb6e4f4020a667dcc40b866db0069ddde
zakiahrzk <127167794+zakiahrzk@users.noreply.github.com>
2023-03-07 12:09:39 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -6,7 +6,7 @@ st.header("My first Streamlit App")
 
 option = st.sidebar.selectbox(
     'Select a mini project',
-     ['line chart','map','T n C'])
+     ['line chart','map','T n C','Long Process'])
 
 if option=='line chart':
     chart_data = pd.DataFrame(
