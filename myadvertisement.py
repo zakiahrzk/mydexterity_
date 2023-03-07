635312af31f1623bd35c0ad0325c43347419efea
zakiahrzk <127167794+zakiahrzk@users.noreply.github.com>
2023-03-07 15:46:52 +0800

    Update myadvertisement.py

--- myadvertisement.py
+++ myadvertisement.py
@@ -1,20 +1,22 @@
-import streamlit as st
-from sklearn.linear_model import LinearRegression
 import numpy as np
 import pandas as pd
+import streamlit as st
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from pandas.io.clipboards import read_clipboard
 
 st.write("""
 # Sales Prediction App
 
-This app predicts the **Sales** using Linear Regression!
+This app predicts the **Sales** !
 """)
 
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
     TV = st.sidebar.slider('TV', 10, 300, 150)
-    Radio = st.sidebar.slider('Radio', 0, 50, 25)
-    Newspaper = st.sidebar.slider('Newspaper', 0, 120, 50)    
+    Radio = st.sidebar.slider('Radio', 0, 50, 30)
+    Newspaper = st.sidebar.slider('Newspaper', 0, 200, 45)    
     data = {'TV': TV,
             'Radio': Radio,
             'Newspaper': Newspaper}
@@ -31,10 +33,18 @@ data = pd.read_csv('Advertising.csv')
 X = data[['TV', 'Radio', 'Newspaper']]
 y = data['Sales']
 
-model = LinearRegression()
-model.fit(X, y)
 
-prediction = model.predict(df)
+Xtrain, Xtest, ytrain, ytest = train_test_split (X,y,test_size = 0.25, random_state = 1) 
 
-st.subheader('The sales prediction is')
+linear = LinearRegression()
+linear.fit(Xtrain, ytrain)
+accuracy = linear.score(Xtest, ytest)
+
+st.subheader('The accuracy of the model is')
+st.write(accuracy)
+
+prediction = linear.predict(df)
+
+st.subheader('Prediction')
 st.write(prediction)
+
